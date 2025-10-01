c6e4f0d8cd2bd380dc6f8d6b3107d10ede3bf17d
dmitryLem <dlemesonok0@gmail.com>
2025-10-01 13:11:28 +0700

    fix task

--- todolist/models.py
+++ todolist/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=50, blank=False, null=False)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     done = models.BooleanField(default=False)
-    date = models.DateTimeField()
+    date = models.DateTimeField(blank=True, null=True)
